cec30a065d7914372279226eeefb78b39f07411c
ERAjeje <42541492+ERAjeje@users.noreply.github.com>
2020-11-25 19:56:06 -0300

    criado client.model

---
+++ src/models/client.model.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+
+
+const { Schema } = mongoose;
+
+const clientSchema = new Schema({
+    name:{
+        type: String,
+        uppercase: true,
+    },
+    cpf: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    cellphone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password:{
+        type: String,
+        required: true,
+        select: false,
+        set: value => crypto.createHash('sha256').update(value).digest('hex'),
+    },
+    },
+    {
+    timestamps: true,
+    toJSON: { virtuals: true, getters: true },
+    toObject: { virtuals: true, getters: true },
+    versionKey: false,
+    }
+);
+
+export const ClientModel = mongoose.model('client', clientSchema);
