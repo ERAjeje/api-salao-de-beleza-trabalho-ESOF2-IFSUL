ed895518e968b92ab99ed51006ebb0c92d46ffac
ERAjeje <42541492+ERAjeje@users.noreply.github.com>
2020-11-25 19:56:38 -0300

    criado procedure.model

---
+++ src/models/procedure.model.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const ProcedureSchema = new Schema({
+    name: {
+        type: String,
+        unique: true,
+        uppercase: true,
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    products: {
+        productId: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        unit: {
+            type: String,
+            required: true,
+        },
+        amount: {
+            type: Number,
+            required: true
+        },
+        required: true,
+    },
+},
+    {
+        timestamps: true,
+        toJSON: { virtuals: true, getters: true },
+        toObject: { virtuals: true, getters: true },
+        versionKey: false,
+    }
+);
+
+export const ProcedureModel = mongoose.model('procedure', ProcedureSchema);
+export const ObjectId = mongoose.type.ObjectId;
