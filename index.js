4913ca8d1cce780cd489ee555da2b35f4c63ebb7
ERAjeje <42541492+ERAjeje@users.noreply.github.com>
2020-11-25 19:52:50 -0300

    Configuração de body-parser e cors

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import v1Route from './src/routes/routes.js';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import { db } from './src/data/mongo.js';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((_, res, next) => {
+	//Qual site tem permissão de realizar a conexão
+    res.header("Access-Control-Allow-Origin", "localhost");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 app.use('/v1', v1Route);
 
 app.listen(3000, async () => {
