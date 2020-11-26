df03149494b64be81022dd97b58444345f16dde4
ERAjeje <42541492+ERAjeje@users.noreply.github.com>
2020-11-25 22:34:45 -0300

    Alter. conf. cors e refat. autentication

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,22 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-import { autentication } from '../utils/autentication.js';
-
-import { RouterTest, Signup, Login, GetUsers, GetLoggedUser } from '../controllers/routesController.js'
-import { UserModel } from '../models/user.model.js';
+import { RouterTest, Signup, Login, GetUsers, GetLoggedUser, autentication } from '../controllers/routesController.js';
 
 const routes = express();
 routes.use(bodyParser.json());
 routes.use(bodyParser.urlencoded({ extended: true }));
-routes.use((_, res, next) => {
-	//Qual site tem permissão de realizar a conexão
-    res.header("Access-Control-Allow-Origin", "localhost");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    routes.use(cors());
-    next();
-});
+routes.use(cors({
+    "origin": "*",
+    "methods": "GET,PUT,POST,DELETE"
+}));
 
 routes.get('/', RouterTest);
 
@@ -25,8 +18,8 @@ routes.post('/signup', Signup);
 
 routes.get('/login', Login);
 
-routes.get('/users', autentication(req, res, next, UserModel), GetUsers);
+routes.get('/users', autentication, GetUsers);
 
-routes.get('/me', autentication(req, res, next, UserModel), GetLoggedUser);
+routes.get('/me', autentication, GetLoggedUser);
 
 export default routes;
